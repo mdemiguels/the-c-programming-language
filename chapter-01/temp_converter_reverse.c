db0a618e2a45cb16e4448904ab0bf965f6b3b385
mdemiguels <miguelschz1997@gmail.com>
2025-06-16 12:55:24 +0200

    chapter 1: add symbolic constants to temperature converter

--- chapter-01/temp_converter_reverse.c
+++ chapter-01/temp_converter_reverse.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
+#define LOWER   0
+#define UPPER   300
+#define STEP    20
+
 int main() {
 
     int fahrenheit;
     float celsius;
 
     printf("Fahrenheit to Celsius Conversion Table: \n");
-    for(fahrenheit = 300; fahrenheit >= 0; fahrenheit = fahrenheit - 20) {
+    for(fahrenheit = UPPER; fahrenheit >= LOWER; fahrenheit = fahrenheit - STEP) {
         celsius = (5.0 / 9.0) * (fahrenheit - 32);
         printf("%3d F \t %6.1f C\n", fahrenheit, celsius);
     }
